c1cace74f324b9528d4ccfa7fb0ce70d727a1da1
NadyaEvp <t_evp@mail.ru>
2024-06-15 12:20:11 +0300

    Update script.js

--- Application/script.js
+++ Application/script.js
@@ -7,21 +7,18 @@ document.getElementById("num").innerHTML = amount;
 document.getElementById("oneSummand").innerHTML = oneSummand;
 
 function guessNum(num) {
-    if (tries >= 3){
-        if (num === twoSummand){
+    if (tries >= 3) {
+        if (num === twoSummand) {
             document.getElementById("twoSummand").innerHTML = twoSummand;
         }
         document.getElementById("myHeading_1").innerHTML = "Игра окончена!"
-        return setTimeout(location.reload (), 30000)
-    }
-    else{
+        location.reload()
+    } else {
         if (num === twoSummand) {
             document.getElementById("myHeading_1").innerHTML = "Правильно!";
             document.getElementById("twoSummand").innerHTML = twoSummand;
-            tries = 3;
-            setTimeout(location.reload (), 30000)
-        }
-        else {
+            location.reload()
+        } else {
             tries++;
             document.getElementById("myHeading_1").innerHTML = "Не правильно!";
             document.getElementById("myHeading_2").innerHTML = "";
